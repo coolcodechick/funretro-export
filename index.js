31b8094d79c9ea48db07e8c072974a4f0bf7cb8b
Tanya B <tanya@coolcodechick.com>
2021-02-02 18:39:14 -0500

    feature/csv: Start basic csv handling.

--- index.js
+++ index.js
@@ -8,6 +8,20 @@ if (!url) {
   throw 'Please provide a URL as the first argument.';
 }
 
+const fileAsArray =file.split('.');
+const fileExt = fileAsArray[fileAsArray.length-1];
+
+const columnDelimiter = fileExt == "csv" ? "," : "";
+const rowDelimiter = "\n";
+
+function removeWhitespace(string) {
+  return string.replace(/\s/g, "");
+}
+
+function hasUpVote(votes) {
+  return parseInt(votes) > 0;
+}
+
 async function run() {
   const browser = await chromium.launch();
   const page = await browser.newPage();
@@ -21,33 +35,47 @@ async function run() {
     throw 'Board title does not exist. Please check if provided URL is correct.'
   }
 
-  let parsedText = boardTitle + '\n\n';
+  let parsedText = boardTitle + rowDelimiter + rowDelimiter;
 
   const columns = await page.$$('.message-list');
 
-  for (let i = 0; i < columns.length; i++) {
-    const columnTitle = await columns[i].$eval('.column-header', (node) => node.innerText.trim());
-
-    const messages = await columns[i].$$('.message-main');
-    if (messages.length) {
-      parsedText += columnTitle + '\n';
+  if (fileExt == "csv" ) {
+    for (let i = 0; i < columns.length; i++) {
+      const columnTitle = await columns[i].$eval('.column-header', (node) => node.innerText.trim());
+      const messages = await columns[i].$$('.message-main');
+      for (let i = 0; i < messages.length; i++) {
+        const messageText = await messages[i].$eval('.message-body .text', (node) => node.innerText.trim());
+        const votes = await messages[i].$eval('.votes .vote-area span.show-vote-count', (node) => node.innerText.trim());
+        if (hasUpVote(votes)) {
+          parsedText +=  columnTitle + columnDelimiter + messageText + columnDelimiter + votes + rowDelimiter;
+        }
+      }
     }
-    for (let i = 0; i < messages.length; i++) {
-      const messageText = await messages[i].$eval('.message-body .text', (node) => node.innerText.trim());
-      const votes = await messages[i].$eval('.votes .vote-area span.show-vote-count', (node) => node.innerText.trim());
-      parsedText += `- ${messageText} (${votes})` + '\n';
-    }
+  } else {
+    for (let i = 0; i < columns.length; i++) {
+      const columnTitle = await columns[i].$eval('.column-header', (node) => node.innerText.trim());
+      const messages = await columns[i].$$('.message-main');
+      if (messages.length) {
+        parsedText += columnTitle + rowDelimiter;
+      }
+      for (let i = 0; i < messages.length; i++) {
+        const messageText = await messages[i].$eval('.message-body .text', (node) => node.innerText.trim());
+        const votes = await messages[i].$eval('.votes .vote-area span.show-vote-count', (node) => node.innerText.trim());
+          parsedText +=  messageText + columnDelimiter + `(${votes})` + rowDelimiter;
+      }
 
-    if (messages.length) {
-      parsedText += '\n';
+      if (messages.length) {
+        parsedText += '\n';
+      }
     }
   }
-
   return parsedText;
 }
 
 function writeToFile(filePath, data) {
-  const resolvedPath = path.resolve(filePath || `../${data.split('\n')[0].replace('/', '')}.txt`);
+  let resolvedPath = fileExt == 'csv' ?
+    path.resolve(`../${removeWhitespace(data.split(rowDelimiter)[0].replace('/', ''))}.csv`) :
+    path.resolve(filePath || `../${removeWhitespace(data.split(rowDelimiter)[0].replace('/', ''))}.txt`);
   fs.writeFile(resolvedPath, data, (error) => {
     if (error) {
       throw error;
